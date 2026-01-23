387267455758a3d74c011ec9ecb127fec20c01ab
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2026-01-23 20:47:55 -0300

    criado prestadorcontroller e corrigida nomenclatura no enum profissao

---
+++ src/main/java/br/unipar/devbackend/fixr/Controller/PrestadorController.java
@@ -0,0 +1,47 @@
+package br.unipar.devbackend.fixr.Controller;
+
+import br.unipar.devbackend.fixr.dto.PrestadorDTO;
+import br.unipar.devbackend.fixr.model.Prestador;
+import br.unipar.devbackend.fixr.service.PrestadorService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/prestador")
+@CrossOrigin(origins = "*")
+public class PrestadorController {
+
+    private final PrestadorService prestadorService;
+
+    public PrestadorController(PrestadorService service){
+        this.prestadorService = service;
+    }
+
+    @PostMapping
+    public Prestador cadastrar(@Valid @RequestBody PrestadorDTO prestadorDTO){
+        return prestadorService.salvar(prestadorDTO);
+    }
+
+    @PutMapping("/{id}")
+    public Prestador atualizar(@PathVariable Long id, @RequestBody PrestadorDTO prestadorDTO){
+        return prestadorService.atualizar(id, prestadorDTO);
+    }
+
+    @GetMapping
+    public List<Prestador> listar(){
+        return prestadorService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Prestador buscarPorId(@PathVariable Long id){
+        return prestadorService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void apagarPrestador(@PathVariable Long id){
+        prestadorService.apagar(id);
+    }
+
+}
