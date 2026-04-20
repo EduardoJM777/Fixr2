a16fe0b86c7d4d6149622776fb7c153af96765f1
Eduardo Johanns Michalski <eduardo160403@edu.unipar.br>
2026-04-20 19:11:32 -0300

    .

--- src/main/java/br/unipar/devbackend/fixr/dto/AnuncioDTO.java
+++ src/main/java/br/unipar/devbackend/fixr/dto/AnuncioDTO.java
@@ -1,6 +1,5 @@
 package br.unipar.devbackend.fixr.dto;
 
-import br.unipar.devbackend.fixr.model.Profissao;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -8,6 +7,17 @@ import jakarta.validation.constraints.Size;
 
 public record AnuncioDTO(
 
+        @NotBlank
+        @NotNull
+        @Size(max = 300)
+        String descricao,
+
+        @NotNull
+        Long idProfissao,
+
+
+        @Positive
+        Long idCliente
 
 
 ) {}
